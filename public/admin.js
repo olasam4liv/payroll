935f6447ecb41e274a304877ec0f86513da69e67
olasam4liv <olasam4liv@gmail.com>
2019-10-13 21:23:05 +0100

    admin view all records feature added

---
+++ public/admin.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+
+    let url ='http://localhost:3000/employee';
+    $.ajax({
+        type: 'GET',
+        url: url,                     
+        success: function(userData){                
+            for (let index = 0; index < userData.length; index++) {   
+                $('#display-user').append(`<tr>
+                <th>${userData[index].id}</th>
+                <td>${userData[index].surname}</td>
+                <td>${userData[index].firstname}</td> 
+                <td>${userData[index].gender}</td>
+                <td>${userData[index].idCardNo}</td>
+                <td> ${userData[index].level}</td>
+                <td>
+                <a href="edit.html?id=${userData[index].id}" data-toggle="tooltip" title="Update user's records. Please becareful" class="btn btn-warning btn-sm">Edit</a> &nbsp;     
+                <a href="viewdetails.html?id=${userData[index].id}" data-toggle="tooltip" title="View details" class="btn btn-primary btn-sm">View</a>  &nbsp;
+                 
+                <a href="paysingle.html?id=${userData[index].id}"  data-toggle="tooltip"    title="Pay Salary" class="btn btn-success btn-sm">Pay</a> &nbsp;
+                <button type="button" class="btn btn-danger btn-sm delete" value="${userData[index].id}"  title="Delete Record" >Delete</button>
+                </tr>`); 
+                $('#amount-modal').append(`<input id="password" type="text" class="form-control" value="${userData[index].surname}">  `)
+           
+             }
+             deleteData();
+          
+        },
+
+        error: function(){
+        console.log('Error')
+        }
+    });
+
+
+
+})
