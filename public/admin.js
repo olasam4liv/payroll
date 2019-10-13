fbd8009840b5550ca6272c28e912d8ba1de0a7f0
olasam4liv <olasam4liv@gmail.com>
2019-10-13 23:03:03 +0100

    fetch single data feature added

--- public/admin.js
+++ public/admin.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
 
-    let url ='http://localhost:3000/employee';
+    //global variable
+    const getUrl = window.location.href
+    let urlId = getUrl.split('id=');
+    let id = parseInt(urlId[1])
+    let url ='http://localhost:3000/employee/';
+
+//fetch all data
     $.ajax({
         type: 'GET',
         url: url,                     
@@ -15,7 +21,7 @@ $(document).ready(function(){
                 <td> ${userData[index].level}</td>
                 <td>
                 <a href="edit.html?id=${userData[index].id}" data-toggle="tooltip" title="Update user's records. Please becareful" class="btn btn-warning btn-sm">Edit</a> &nbsp;     
-                <a href="viewdetails.html?id=${userData[index].id}" data-toggle="tooltip" title="View details" class="btn btn-primary btn-sm">View</a>  &nbsp;
+                <a href="adminviewdetails.html?id=${userData[index].id}" data-toggle="tooltip" title="View details" class="btn btn-primary btn-sm">View</a>  &nbsp;
                  
                 <a href="paysingle.html?id=${userData[index].id}"  data-toggle="tooltip"    title="Pay Salary" class="btn btn-success btn-sm">Pay</a> &nbsp;
                 <button type="button" class="btn btn-danger btn-sm delete" value="${userData[index].id}"  title="Delete Record" >Delete</button>
@@ -32,7 +38,83 @@ $(document).ready(function(){
         }
     });
 
+// fetch single data
+$.ajax({
+    type: 'GET',
+    url: url + id,                     
+    success: function(data){                
+        $('#view-details').append(` <tbody>
+        <tr>
+          <th scope="row">Staff ID No:</th>
+          <td colspan="3">${data.idCardNo}</td>
+        </tr>
+        <tr>
+          <th scope="row">Suname:</th>
+          <td colspan="3">${data.surname}</td>
+        </tr>
+        <tr>
+          <th scope="row">First Name</th>
+          <td colspan="3">${data.firstname}</td>
+        </tr>
+        <tr>
+          <th scope="row">Other Name</th>
+          <td colspan="3">${data.otheranme}</td>                             
+        </tr>
+          <tr>
+              <th scope="row">Date of Birth:</th>
+              <td colspan="3">${data.dob}</td>
+          </tr>
+          <tr>
+              <th scope="row">Email</th>
+              <td colspan="3">${data.email}</td>
+            </tr>
+            <tr>
+              <th scope="row">Contact Number</th>
+              <td colspan="3">${data.phonenumber}</td>
+            </tr>
+            <tr>
+              <th scope="row">Address</th>
+              <td colspan="3">${data.contact_address}</td>
+            </tr>
+            <tr>
+              <th scope="row">Gender</th>
+              <td colspan="3">${data.gender}</td>                             
+            </tr>
+            <tr>
+                  <th scope="row">Level:</th>
+                  <td colspan="3">${data.level}</td>
+             </tr>
+            <tr>
+                  <th scope="row">Salary Amount:</th>
+                  <td colspan="3">&#8358 ${data.salary}</td>
+             </tr>
+             <tr>
+                  <th scope="row">Last Amount Received</th>
+                  <td colspan="3">&#8358 ${data.lastAmountReceived}</td>
+                <tr>
+                tr>
+                  <th scope="row">Regisration Date </th>
+                  <td colspan="3">${data.regDate}</td>
+                </tr>
+                <tr>
+                  <th scope="row">Last Payment Date</th>
+                  <td colspan="3">${data.lastPaymentDate}</td>
+                </tr>
+   
+                <tr>
+                  <th scope="row">Account Status</th>
+                  <td colspan="3">${data.isActive}</td>
+                </tr>
+                
+                      
+      </tbody>`);
+      
+    },
 
+    error: function(){
+    console.log('Error')
+    }
+});
      //delete
  function deleteData(){
     $("button.delete").click( function(e) {
