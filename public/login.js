6409f725243607510ff4aab0064f51b31ca1c24e
olasam4liv <olasam4liv@gmail.com>
2019-10-24 17:33:59 +0100

    login bug fixed

--- public/login.js
+++ public/login.js
@@ -1,35 +1,31 @@
 $(document).ready(function(){
+  localStorage.clear();
    let url ='http://localhost:3000/employee';
     $('#login-form').on('submit', function(e){
         e.preventDefault();
         let email = $('#email').val();
         let password = $('#password').val(); 
-
-    $.get(url, function(userData){
-      
+    $.get(url, function(userData){ 
       for (let object of userData) {        
-        if (object.email === email && object.password === password && object.isAdmin ==="Yes" && object.isActive ==="Active") {          
-          let id = object.id;
-          id = parseInt(id);
-          localStorage.setItem('userID', id);
-           window.location.replace('admindashboard.html');
-        
-          return;
-        } if(object.email === email && object.password === password && object.isAdmin ==="No" && object.isActive === "Active"){  
+        if (object.email === email && object.password === password && object.isAdmin ==="Yes" && object.isActive ==="Yes") {          
           // let id = object.id;
           // id = parseInt(id);
-
+          console.log(object.surname)
+          // localStorage.setItem('userID', id);
+           window.location.replace('admindashboard.html');        
+          return;
+        }  if(object.email === email && object.password === password && object.isAdmin ==="No" && object.isActive === "Yes"){  
+          // let id = object.id;
+          // id = parseInt(id);
+          console.log(object.id)
           // localStorage.setItem('userID', id);
            window.location.replace('dashboard.html');
-           return;
-        }
+          return;
+        } 
       }
-      //  return false
-      alert('Invalid email or password')
+       
+     alert('Invalid email or password')
   
-      });        
-        
-
-    
+      });   
     });
 })
